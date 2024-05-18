af8416263e9303911764bf1f592afdfb2692f610
Dominik Pilipczuk <63516830+snickerdoodle2@users.noreply.github.com>
2024-05-18 14:45:45 +0200

    fix: buttons :)

--- src/components/ui/Background.tsx
+++ src/components/ui/Background.tsx
@@ -4,10 +4,10 @@ const Background = ({ children }) => {
     return (
         <div
             className='
-    bg-gradient-to-br from-sky-500 from-10% via-blue-700 via-30% to-purple-500 to-90% h-screen'
+    bg-gradient-to-br from-sky-500 from-10% via-blue-700 via-30% to-purple-500 to-90% h-screen flex flex-col'
         >
             <div className="h-screen w-screen absolute bg-[url('/assets/Starts.png')]"></div>
-            <div className='relative z-10 h-screen flex flex-col'>
+            <div className='relative z-10 h-screen flex flex-col flex-1 py-12 px-6'>
                 {children}
             </div>
         </div>
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -15,32 +15,30 @@ import Background from '@/components/ui/Background';
 const Home = () => {
     return (
         <Background>
-            <div className=' py-12 px-6 '>
-                <CardTitle className='py-4 px-6 drop-shadow-2xl text-slate-50 text-center'>
-                    Strona Główna
-                </CardTitle>
+            <CardTitle className='py-4 px-6 drop-shadow-2xl text-slate-50 text-center'>
+                Strona Główna
+            </CardTitle>
 
-                <Card>
-                    <CardHeader>
-                        <CardTitle>Card Title</CardTitle>
-                        <CardDescription>Card Description</CardDescription>
-                    </CardHeader>
-                    <CardContent>
-                        <p>Card Content</p>
-                    </CardContent>
-                    <CardFooter>
-                        <p>Card Footer</p>
-                    </CardFooter>
-                </Card>
-                <div className='p-8 flex justify-between'>
-                    <Button variant='outline' size='icon'>
-                        <ChevronLeft className='h-4 w-4' />
-                    </Button>
+            <Card>
+                <CardHeader>
+                    <CardTitle>Card Title</CardTitle>
+                    <CardDescription>Card Description</CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <p>Card Content</p>
+                </CardContent>
+                <CardFooter>
+                    <p>Card Footer</p>
+                </CardFooter>
+            </Card>
+            <div className='p-8 flex justify-between absolute w-screen left-1/2 -translate-x-1/2 bottom-10 z-10'>
+                <Button variant='outline' size='icon'>
+                    <ChevronLeft className='h-4 w-4' />
+                </Button>
 
-                    <Button variant='outline' size='icon'>
-                        <ChevronRight className='h-4 w-4' />
-                    </Button>
-                </div>
+                <Button variant='outline' size='icon'>
+                    <ChevronRight className='h-4 w-4' />
+                </Button>
             </div>
             <img src='assets/bottom.png' className='absolute bottom-0 '></img>
             <img
