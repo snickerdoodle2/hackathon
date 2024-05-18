6d7570981d732e2c14c66f9ff566f034d6cc502c
Dominik Pilipczuk <63516830+snickerdoodle2@users.noreply.github.com>
2024-05-18 11:29:48 +0200

    feat: better filtering

--- src/components/minigames/wordle/wordle.tsx
+++ src/components/minigames/wordle/wordle.tsx
@@ -1,5 +1,5 @@
 import { Input } from '@/components/ui/input';
-import { useState } from 'react';
+import React, { useState } from 'react';
 
 type Paradigm = 'Objective' | 'Imperative' | 'Functional';
 type Language = {
@@ -27,12 +27,20 @@ const langs: Language[] = [
     },
 ];
 
+const Results: React.FC<{ choices: Language[] }> = ({ choices }) => {
+    return <p>{choices.map((e) => e.name).join(' ')}</p>;
+};
+
 export const Wordle = () => {
     const [input, setInput] = useState('');
 
+    const [choices, setChoices] = useState<Language[]>([]);
+
     const selectLanguage = (name: string) => {
         setInput('');
-        console.log(name);
+        const lang = langs.find((e) => e.name === name);
+        if (!lang) return;
+        setChoices((prev) => [...prev, lang]);
     };
 
     return (
@@ -45,14 +53,27 @@ export const Wordle = () => {
             />
             {input.length > 0 &&
                 langs
-                    .filter((e) =>
-                        e.name.toLowerCase().startsWith(input.toLowerCase())
-                    )
+                    .filter((e) => {
+                        if (
+                            !e.name
+                                .toLowerCase()
+                                .startsWith(input.toLowerCase())
+                        )
+                            return false;
+
+                        // hide already selected
+                        if (choices.find((c) => c.name === e.name))
+                            return false;
+
+                        return true;
+                    })
                     .map((e) => (
                         <p key={e.name} onClick={() => selectLanguage(e.name)}>
                             {e.name}
                         </p>
                     ))}
+
+            <Results choices={choices} correct={langs[0]} />
         </div>
     );
 };
