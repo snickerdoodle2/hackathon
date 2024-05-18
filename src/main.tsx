723155507c589c43f60bf879e1cb6f8355eb1b97
Dominik Pilipczuk <63516830+snickerdoodle2@users.noreply.github.com>
2024-05-18 10:02:19 +0200

    feat: react-router

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+// TODO: jakies inne routy moze?
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <p>Home</p>
+    },
+    {
+        path: "scoreboard",
+        element: <p>Scoreboard</p>
+    },
+    {
+        path: "*",
+        // TODO: jak bedzie czas to spoko to bedzie zrobic, ale pewnie nie bedzie
+        // i to tak whatever
+        element: <p>404 - not found :(</p>
+    }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <p>Hello, world!</p>
+        <RouterProvider router={router} />
     </React.StrictMode>
 );
