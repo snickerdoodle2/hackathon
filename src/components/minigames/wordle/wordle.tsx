10084cf5a2a7d3b2d098ad4bd5e39a4c37b27765
Dominik Pilipczuk <63516830+snickerdoodle2@users.noreply.github.com>
2024-05-18 11:57:32 +0200

    feat: results grid

--- src/components/minigames/wordle/wordle.tsx
+++ src/components/minigames/wordle/wordle.tsx
@@ -1,4 +1,5 @@
 import { Input } from '@/components/ui/input';
+import { cn } from '@/lib/utils';
 import React, { useState } from 'react';
 
 type Paradigm = 'Objective' | 'Imperative' | 'Functional';
@@ -27,8 +28,45 @@ const langs: Language[] = [
     },
 ];
 
-const Results: React.FC<{ choices: Language[] }> = ({ choices }) => {
-    return <p>{choices.map((e) => e.name).join(' ')}</p>;
+const Cell: React.FC<{ text: string; className?: string }> = ({
+    text,
+    className = 5,
+}) => {
+    return (
+        <p
+            className={cn(
+                'border aspect-square text-center justify-center items-center flex rounded-lg',
+                className
+            )}
+        >
+            {text}
+        </p>
+    );
+};
+
+const Results: React.FC<{ choices: Language[]; correct: Language }> = ({
+    choices,
+}) => {
+    return (
+        <div className='grid grid-cols-5 text-[0.7rem] font-semibold break-words gap-1'>
+            <Cell text='Język' className='font-bold' />
+            <Cell text='Paradygmat' className='font-bold' />
+            <Cell text='Typowany' className='font-bold' />
+            <Cell text='Compiled?' className='font-bold' />
+            <Cell text='Rok wydania' className='font-bold' />
+            {choices.map((lang) => {
+                return (
+                    <>
+                        <Cell text={lang.name} />
+                        <Cell text={lang.paradigm.join(', ')} />
+                        <Cell text={lang.typed} />
+                        <Cell text={lang.complied} />
+                        <Cell text={'' + lang.year} />
+                    </>
+                );
+            })}
+        </div>
+    );
 };
 
 export const Wordle = () => {
@@ -44,34 +82,39 @@ export const Wordle = () => {
     };
 
     return (
-        <div>
-            <Input
-                value={input}
-                onChange={(e) => {
-                    setInput(e.target.value);
-                }}
-            />
-            {input.length > 0 &&
-                langs
-                    .filter((e) => {
-                        if (
-                            !e.name
-                                .toLowerCase()
-                                .startsWith(input.toLowerCase())
-                        )
-                            return false;
+        <div className='flex flex-col justify-between h-svh'>
+            <div>
+                <Input
+                    value={input}
+                    onChange={(e) => {
+                        setInput(e.target.value);
+                    }}
+                />
+                {input.length > 0 &&
+                    langs
+                        .filter((e) => {
+                            if (
+                                !e.name
+                                    .toLowerCase()
+                                    .startsWith(input.toLowerCase())
+                            )
+                                return false;
 
-                        // hide already selected
-                        if (choices.find((c) => c.name === e.name))
-                            return false;
+                            // hide already selected
+                            if (choices.find((c) => c.name === e.name))
+                                return false;
 
-                        return true;
-                    })
-                    .map((e) => (
-                        <p key={e.name} onClick={() => selectLanguage(e.name)}>
-                            {e.name}
-                        </p>
-                    ))}
+                            return true;
+                        })
+                        .map((e) => (
+                            <p
+                                key={e.name}
+                                onClick={() => selectLanguage(e.name)}
+                            >
+                                {e.name}
+                            </p>
+                        ))}
+            </div>
 
             <Results choices={choices} correct={langs[0]} />
         </div>
