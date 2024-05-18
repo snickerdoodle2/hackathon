e5f62555b4e2dd82c927644cde4007d2033cfef3
Wiktor Gut <411761@student.agh.edu.pl>
2024-05-18 15:21:36 +0200

    nonogram

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Nono from './components/minigames/Nono';
 
 // TODO: jakies inne routy moze?
 const router = createBrowserRouter([
@@ -12,6 +13,10 @@ const router = createBrowserRouter([
     {
         path: 'scoreboard',
         element: <p>Scoreboard</p>,
+    },
+    {
+        path: 'nono',
+        element: <Nono onFinish={()=>alert("xd")}/>
     },
     {
         path: '*',
