1b4c5dde632b19ff4caac8362c20189886e72ec6
Łukasz Kolber <kolberu@gmail.com>
2024-05-19 11:45:04 +0200

    fix(eslint)

--- src/lib/section.ts
+++ src/lib/section.ts
@@ -22,6 +22,7 @@ type Configuration = {
     name: string;
     localizationTips: string;
     localization: string;
+    password: string;
     tasks: Task[];
 };
 
@@ -42,6 +43,26 @@ class Section {
         const configData = (await configuration.json()) as Configuration;
 
         return new Section(id, configData);
+    }
+
+    authorizeSection(password: string) {
+        if (this.configData === null)
+            throw new Error('Section not initialized');
+
+        if (this.configData.password !== password) {
+            console.log('Wrong password', this.configData.password, password);
+            return false;
+        }
+
+        console.log(
+            'Access granted',
+            this.configData.password,
+            password,
+            this.configData.password == password
+        );
+        localStorage.setItem(`section-${this.configurationid}`, 'true');
+
+        return true;
     }
 
     getTaskById(id: number) {
